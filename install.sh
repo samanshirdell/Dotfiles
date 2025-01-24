49bf19d3c78c9845798902764719318a8e38aded
Eli Fouts <eligfouts@gmail.com>
2025-01-24 01:03:33 -0500

    New thing

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ yay -S reflector rsync
 sudo reflector --country 'US' --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 yay -S pywal swww
 wal -i ~/Dotfiles/wallpapers/walls/r82.jpg -n
-yay -S waybar swaync starship nerdfetch neovim python-pywalfox hypridle hyprpicker hyprlock thunar wlogout fd
+yay -S waybar swaync starship nerdfetch neovim python-pywalfox hypridle hyprpicker hyprlock thunar wlogout fd cava
 yay -S nerd-fonts
 yay -S nwg-look qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin
 yay -S thunar lsd gvfs
@@ -15,6 +15,5 @@ yay -S pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol
 systemctl --user enable pipewire.service pipewire-pulse.service
 systemctl --user start pipewire.service pipewire-pulse.service
 sudo cp -f -r ~/Dotfiles/wallpapers ~/
-wal -i ~/Dotfiles/wallpapers/walls/Aloe.jpg
 sudo cp -r -f ~/Dotfiles/.config/* ~/.config/
 sudo cp -r -f ~/Dotfiles/.bashrc ~/
