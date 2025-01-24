60720e3f8e22a818650fd149c83627acf382e3df
Eli Fouts <eligfouts@gmail.com>
2025-01-24 12:42:29 -0500

    Big Update! New system

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -76,13 +76,13 @@ return {
             }))
         dashboard.section.buttons.val = {
             dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-            dashboard.button( "f", "  > Find file", ":lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files' } })<CR>"),
+            dashboard.button( "f", "󰱼  > Find file", ":lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files' } })<CR>"),
             dashboard.button( "F", "󰥨  > Find folder", ":lua search_and_scope_into_directory()<CR>"),
             dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
             dashboard.button( "c", "  > Config" , ":cd ~/.config/nvim | Telescope find_files<CR>"),
             dashboard.button( "l", "󰒲 > Lazy", ":Lazy<CR>"),
             dashboard.button( "h", "  > Settings" , ":cd ~/.config/hypr | Telescope find_files<CR>"),
-            dashboard.button( "q", "  > Quit", ":qa<CR>"),
+            dashboard.button( "q", "  > Quit", ":qa<CR>"),
 
 
         }
