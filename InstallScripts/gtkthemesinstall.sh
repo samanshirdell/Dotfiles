253b441f4697ccf0773fe34db739e4e430f93c06
Eli Fouts <eligfouts@gmail.com>
2025-01-25 00:17:17 -0500

    Update gtkthemesinstall.sh

--- InstallScripts/gtkthemesinstall.sh
+++ InstallScripts/gtkthemesinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 yay -S nwg-look qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin
 echo "___________________________________________"
 echo "Use nwg-look to set gtk themes for hyprland"
