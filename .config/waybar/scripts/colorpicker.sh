05fa7db8ac1d98e1cfdb96667b4a39ba3bc0405e
Eli Fouts <eligfouts@gmail.com>
2025-08-26 14:51:05 -0400

    Update colorpicker.sh

--- .config/waybar/scripts/colorpicker.sh
+++ .config/waybar/scripts/colorpicker.sh
@@ -18,6 +18,7 @@ limit=10
 
 [[ $# -eq 1 && $1 = "-j" ]] && {
   text="$(head -n 1 "$loc/colors")"
+  text=${text:-#FFFFFF} #if we start for the first time ever the file is empty and thus waybar will throw an error and not display the colorpicker. here is a fallback for that
 
   mapfile -t allcolors < <(tail -n +2 "$loc/colors")
   # allcolors=($(tail -n +2 "$loc/colors"))
@@ -40,15 +41,22 @@ check hyprpicker || {
   exit
 }
 killall -q hyprpicker
-color=$(hyprpicker)
+color=$(hyprpicker | grep -v "^\[ERR\]")
+[[ -n $color ]] || exit
 
 check wl-copy && {
   echo "$color" | sed -z 's/\n//g' | wl-copy
 }
 
 prevColors=$(head -n $((limit - 1)) "$loc/colors")
+
+source ~/.cache/wal/colors.sh && color_preview=$wallpaper
+check magick && {
+  magick -size 64x64 canvas:"$color" "$loc/color_preview.png"
+  color_preview="$loc/color_preview.png"
+}
 echo "$color" >"$loc/colors"
 echo "$prevColors" >>"$loc/colors"
 sed -i '/^$/d' "$loc/colors"
-source ~/.cache/wal/colors.sh && notify-send "Color Picker" "This color has been selected: $color" -i $wallpaper
+notify-send "Color Picker" "This color has been selected: $color" -i $color_preview
 pkill -RTMIN+1 waybar
