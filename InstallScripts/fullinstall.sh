32d8d97145b4e472b246b132250b75f2dd9dead9
Eli Fouts <eligfouts@gmail.com>
2025-08-26 11:28:47 -0400

    Update fullinstall.sh

--- InstallScripts/fullinstall.sh
+++ InstallScripts/fullinstall.sh
@@ -12,7 +12,7 @@ fi
 # Automatic install section
 if [[ $install_choice == "a" ]]; then
     sudo chmod -R 777 $HOME
-    yay -S reflector rsync python-pywal16 swww waybar swaync starship myfetch neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl clock-rs-git nerd-fonts nwg-look qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin thunar gvfs tumbler eza bottom htop libreoffice-fresh spotify ncspot discord code blueman bluez pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol pulsemixer gnome-network-displays gst-plugins-bad
+    yay -S reflector rsync python-pywal16 swww waybar swaync starship myfetch neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pacman-contrib pyprland wlogout fd cava brightnessctl clock-rs-git nerd-fonts nwg-look qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin thunar gvfs tumbler eza bottom htop libreoffice-fresh spotify ncspot discord code blueman bluez pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol pulsemixer gnome-network-displays gst-plugins-bad
     sudo reflector --country 'US' --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
     systemctl enable bluetooth
     systemctl --user enable pipewire.service
